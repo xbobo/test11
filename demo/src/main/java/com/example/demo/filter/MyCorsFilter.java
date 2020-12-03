4713572575a1164fbdfe90d041c4b7dd15761655
xiaobo <603786093@qq.com>
2020-12-03 14:05:20 +0800

    test

--- demo/src/main/java/com/example/demo/filter/MyCorsFilter.java
+++ demo/src/main/java/com/example/demo/filter/MyCorsFilter.java
@@ -1,15 +1,9 @@
 package com.example.demo.filter;
 
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
-import javax.servlet.annotation.WebFilter;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class MyCorsFilter extends CorsFilter {
@@ -17,4 +11,5 @@ public class MyCorsFilter extends CorsFilter {
     public MyCorsFilter(CorsConfigurationSource configSource) {
         super(configSource);
     }
+
 }
